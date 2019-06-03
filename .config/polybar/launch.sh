c8bd66277b65d63944e81647ea81c721923bce27
luc1ph3r <electrocartridge@gmail.com>
2019-06-03 23:46:43 +0300

    [i3/polybar]: Use polybar instead of i3bar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -x
+
+(
+  flock 200
+
+  killall -q polybar
+
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output="eDP-1"
+
+  for m in $outputs; do
+    if [[ "$m" == "HDMI-2" ]];
+    then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]];
+    then
+      TRAY_POSITION=right
+    fi
+
+    polybar --reload example </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+  done
+) 200>/var/tmp/polybar-launch.lock
